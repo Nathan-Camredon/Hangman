a79bf901265b4ee0f23cc49458666d57f199407a
Marius GAVINI <marius.gavini@laplateforme.io>
2026-01-20 15:35:27 +0100

    [Modify]= function load_words + add + del and modify tests

--- modules/words_list.py
+++ modules/words_list.py
@@ -1,11 +1,17 @@
+import os
+from colored import Fore, Back, Style
+
 def load_words():
-    #Load a list of words from a file and return them as a list.
-    with open('words.txt', 'r', encoding='utf-8') as file:
-        words = [line.strip() for line in file]
-    return words
+    base_dir = os.path.dirname(os.path.abspath(__file__)) 
+    file_path = os.path.join(base_dir, '..', 'data', 'words.txt')        
+    words = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            words.append(line.strip().lower())
+    return words, file_path
 
 def filter_words_by_length(words, length):
-    #Filter the list of words to include only those of a specific length for different difficulty.
     difficulty_list = []
     if length > 6:
         for word in words:
@@ -18,26 +24,36 @@ def filter_words_by_length(words, length):
                 difficulty_list.append(word)
     return difficulty_list
 
-def add_word(words, new_word):
-    #Add a new word to the list if it is not already present.
+def add_word(words, new_word, file_path):
     if new_word not in words:
-        with open('words.txt', 'a', encoding='utf-8') as file:
-            file.write(f"{new_word}\n")
-        words.append(new_word)
-    print(words)
+        with open(file_path, 'a', encoding='utf-8') as file:
+            file.write(f"{new_word.lower()}\n")
+        words.append(new_word.lower())
     return words
 
-def delete_word(words, word_to_delete):
-    #Delete a word from the list if it exists.
+def delete_word(words, word_to_delete, file_path):
+    word_to_delete = word_to_delete.lower().strip()
     if word_to_delete in words:
         words.remove(word_to_delete)
-        with open('words.txt', 'w', encoding='utf-8') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
                 for word in words:
                     file.write(f"{word}\n")
     return words
 
 #test
-if __name__ == "__main__" :
-    filter_words_by_length(load_words(), 5)
-    add_word(load_words(), "example")
-    delete_word(load_words(), "example")
+if __name__ == "__main__":
+
+    Fore.red
+    Back.red
+    Style.reset
+    Fore.rgb('100%', '50%', '30%')
+    words, file_path = load_words()
+
+    print(f"{Fore.white}{Back.green}Filtrage :{Style.reset}", filter_words_by_length(words, 5))
+    print(f"{Fore.white}{Back.green}Filtrage hard:{Style.reset}", filter_words_by_length(words, 7))
+
+    words = add_word(words, "example", file_path)
+    print(f"{Fore.white}{Back.green}Après ajout :{Style.reset}", words)
+
+    words = delete_word(words, "example", file_path)
+    print(f"{Fore.white}{Back.green}Après suppression :{Style.reset}", words)
